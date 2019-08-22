a737ce928ad98de2654b733d2c4c0feb92d77b70
Jonathan Muir <jonathan.muir@civica.co.uk>
2019-08-22 11:17:40 +0100

    added templates to configuration

--- extension.js
+++ extension.js
@@ -21,6 +21,18 @@ let insertText = value => {
   });
 };
 
+let getImageTemplate = () => {
+  return vscode.workspace.getConfiguration("staticSiteHero")[
+    "imagePathTemplate"
+  ];
+};
+
+let getFileTemplate = () => {
+  return vscode.workspace.getConfiguration("staticSiteHero")[
+    "filePathTemplate"
+  ];
+};
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -46,7 +58,11 @@ function activate(context) {
       vscode.window
         .showQuickPick(linkTypeList, { placeHolder: "Link Type" })
         .then(result => {
-          insertText(result);
+          if (result === "File") {
+            insertText(getFileTemplate());
+          } else if (result === "Image") {
+            insertText(getImageTemplate());
+          }
         });
     }
   );
